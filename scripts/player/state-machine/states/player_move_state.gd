40269ecb987614b887a54e8dabd23192789b0061
Kevin Pfeifle <kevinpfeifle@outlook.com>
2024-10-21 00:16:21 -0400

    added functional y-sorting for the player against the tilemap and walls, added more tile assets

--- scripts/player/state-machine/states/player_move_state.gd
+++ scripts/player/state-machine/states/player_move_state.gd
@@ -1,11 +1,17 @@
 class_name PlayerMoveState
 extends PlayerState
 
+var player_scale: Vector2
+var player_scale_inv: Vector2
+
 func enter(args: Array) -> void:
 	super(args)
 	# Adds this back in once animations are ready.
 	if character.animation_player.current_animation != "move":
 		character.animation_player.play("move")
+	player_scale = abs(character.sprite.scale)
+	player_scale_inv = player_scale * -1
+
 
 func physics_update(_delta):
 	var input_x: float = Input.get_axis("player_left", "player_right")
@@ -15,9 +21,9 @@ func physics_update(_delta):
 	character.velocity.y = input_y * character.SPEED
 	
 	if character.velocity.x < 0:
-		character.sprite.scale.x = 1
+		character.sprite.scale.x = player_scale.x
 	elif character.velocity.x > 0:
-		character.sprite.scale.x = -1
+		character.sprite.scale.x = player_scale_inv.x
 
 	character.move_and_slide()
 
